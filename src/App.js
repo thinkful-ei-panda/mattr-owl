def966bf9634f0a826ceae5bb164341c34ad85cb
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-17 10:46:04 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { participants, chatEvents } from "./data";
+import ParticipantList from "./ParticipantList";
+import Chat from './Chat';
+import Stage from "./Stage";
+import ".App.css";
+
+export default function App(props) {
+  return (
+    <div className="app">
+      <Chat events={chatEvents} participants={participants}/>
+      <Stage participants={participants} />
+    </div>
+  );
+}
